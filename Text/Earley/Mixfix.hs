011c80049af3497685324b2e70b7b751e6a8e4f1
Olle Fredriksson <fredriksson.olle@gmail.com>
2015-08-12 21:34:35 +0200

    Mixfix: Allow separate semantic actions

--- Text/Earley/Mixfix.hs
+++ Text/Earley/Mixfix.hs
@@ -3,14 +3,15 @@ module Text.Earley.Mixfix
   ( Associativity(..)
   , Holey
   , mixfixExpression
+  , mixfixExpressionSeparate
   ) where
 
 #if !MIN_VERSION_base(4,8,0)
 import Control.Applicative
+import Data.Traversable(sequenceA)
 #endif
 import Data.Either
 import Data.Foldable(asum, foldrM)
-import Data.Traversable(sequenceA)
 import Text.Earley
 
 replicateA :: Applicative f => Int -> f a -> f [a]
@@ -52,12 +53,32 @@ mixfixExpression
   -- ^ How to combine the successful application of a holey identifier to its
   -- arguments into an expression.
   -> Grammar r e (Prod r e t expr)
-mixfixExpression table atom app = mdo
+mixfixExpression table atom app = mixfixExpressionSeparate table' atom
+  where
+    table' = [[(holey, assoc, app) | (holey, assoc) <- row] | row <- table]
+
+-- | A version of 'mixfixExpression' with a separate semantic action for each
+-- individual 'Holey' identifier.
+mixfixExpressionSeparate
+  :: [[(Holey (Prod r e t ident), Associativity, Holey ident -> [expr] -> expr)]]
+  -- ^ A table of holey identifier parsers, with associativity information and
+  -- semantic actions.  The identifiers should be in groups of precedence
+  -- levels listed from binding the least to the most tightly.
+  --
+  -- The associativity is taken into account when an identifier starts or ends
+  -- with holes, or both. Internal holes (e.g. after "if" in "if_then_else_")
+  -- start from the beginning of the table.
+  --
+  -- Note that this rule also applies to identifiers with multiple consecutive
+  -- holes, e.g. "if__" --- the associativity then applies to both holes.
+  -> Prod r e t expr
+  -- ^ An atom, i.e. what is parsed at the lowest level. This will
+  -- commonly be a (non-mixfix) identifier or a parenthesised expression.
+  -> Grammar r e (Prod r e t expr)
+mixfixExpressionSeparate table atom = mdo
   expr <- foldrM ($) atom $ map (level expr) table
   return expr
   where
-    app' xs = app (concatMap (either (map $ const Nothing) $ map Just) xs)
-            $ concat $ lefts xs
     level expr idents next = mdo
       same <- rule $ asum $ next : map (mixfixIdent same) idents
       return same
@@ -71,8 +92,10 @@ mixfixExpression table atom app = mdo
           Right ps':rest -> Right (consA p ps')       : rest
           rest           -> Right (consA p $ pure []) : rest
 
-        mixfixIdent same (ps, a) = app' <$> go (grp ps)
+        mixfixIdent same (ps, a, f) = f' <$> go (grp ps)
           where
+            f' xs = f (concatMap (either (map $ const Nothing) $ map Just) xs)
+                   $ concat $ lefts xs
             go ps' = case ps' of
               [] -> pure []
               [Right p] -> pure . Right <$> p
