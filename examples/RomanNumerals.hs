a0c289a281ba47397cc2b3412e7a7599726b0208
Tero Keinänen <tero.keinanen@avarko.com>
2016-12-30 21:33:04 +0200

    Add reworked Roman numeral example and executable

--- examples/RomanNumerals.hs
+++ examples/RomanNumerals.hs
@@ -1,85 +1,62 @@
 {-# LANGUAGE RecursiveDo #-}
 module Main where
 
-import Control.Applicative ((<*), (<|>), some)
+import Control.Applicative ((<|>), (<**>))
 import System.Environment (getArgs)
 import Text.Earley
 
-data RomanSymbol = I | IV | V | IX | X | XL | L | XC | C | CD | D | CM | M
-  deriving (Show, Read)
+numeral :: String -> Int -> Prod r String Char Int
+numeral str n = n <$ list str
 
-type RomanNumeral = [RomanSymbol]
-
-symbolToDec :: RomanSymbol -> Int
-symbolToDec I = 1
-symbolToDec IV = 4
-symbolToDec V = 5
-symbolToDec IX = 9
-symbolToDec X = 10
-symbolToDec XL = 40
-symbolToDec L = 50
-symbolToDec XC = 90
-symbolToDec C = 100
-symbolToDec CD = 400
-symbolToDec D = 500
-symbolToDec CM = 900
-symbolToDec M = 1000
-
-romanNumeralToDec :: RomanNumeral -> Int
-romanNumeralToDec = sum . map symbolToDec
-
-type RomanNumeralProd r = Prod r RomanSymbol Char RomanNumeral
-
-parseMany :: Int -> RomanSymbol -> RomanNumeralProd r
-parseMany n s = replicate n s <$ (list $ concat $ replicate n (show s))
-
-parseSingle :: RomanSymbol -> RomanNumeralProd r
-parseSingle = parseMany 1
-
-parseSome :: RomanSymbol -> RomanNumeralProd r
-parseSome = fmap concat . some . parseSingle
-
-andThen :: RomanNumeralProd r -> RomanNumeralProd r -> RomanNumeralProd r
-andThen a b = fmap (++) a <*> b
-
-andThenAny :: RomanNumeralProd r -> [RomanNumeralProd r] -> RomanNumeralProd r
-andThenAny a = foldl1 (<|>) . map (a `andThen`)
-
-romanNumeralsGrammar :: Grammar r (RomanNumeralProd r)
+romanNumeralsGrammar :: Grammar r (Prod r String Char Int)
 romanNumeralsGrammar = mdo
+
+  thousands <- rule 
+    $ pure 0 
+    <|> numeral "M" 1000 <**> fmap (+) thousands
+
+  le300 <- rule 
+    $ pure 0 
+    <|> numeral "C" 100 
+    <|> numeral "CC" 200 
+    <|> numeral "CCC" 300
+
+  hundreds <- rule 
+    $ le300 
+    <|> numeral "CD" 400 
+    <|> numeral "D" 500 <**> fmap (+) le300 
+    <|> numeral "CM" 900
+
+  le30 <- rule
+    $ pure 0
+    <|> numeral "X" 10
+    <|> numeral "XX" 20
+    <|> numeral "XXX" 30
+
+  tens <- rule 
+    $ le30
+    <|> numeral "XL" 40
+    <|> numeral "L" 50 <**> fmap (+) le30
+    <|> numeral "XC" 90
+
+  le3 <- rule
+    $ pure 0
+    <|> numeral "I" 1
+    <|> numeral "II" 2
+    <|> numeral "III" 3
+
+  units <- rule
+    $ le3
+    <|> numeral "IV" 4
+    <|> numeral "V" 5 <**> fmap (+) le3
+    <|> numeral "IX" 9
+
+  return
+    $ thousands 
+    <**> fmap (+) hundreds 
+    <**> fmap (+) tens 
+    <**> fmap (+) units
   
-  i <- rule $ parseMany 3 I <|> parseMany 2 I <|> parseSingle I <?> I
-
-  iv <- rule $ parseSingle IV <?> IV
-
-  v <- rule $ parseSingle V <|> parseSingle V `andThen` i <?> V
-
-  ix <- rule $ parseSingle IX <?> IX
-
-  x' <- rule $ parseMany 3 X <|> parseMany 2 X <|> parseSingle X
-  x <- rule $ x' <|> x' `andThenAny` [ix, v, iv, i] <?> X
-
-  xl <- rule $ parseSingle XL <|> parseSingle XL `andThenAny` [ix, v, iv, i] <?> XL
-
-  l <- rule $ parseSingle L <|> parseSingle L `andThenAny` [x, ix, v, iv, i] <?> L
-
-  xc <- rule $ parseSingle XC <|> parseSingle XC `andThenAny` [ix, v, iv, i] <?> XC
-
-  c' <- rule $ parseMany 3 C <|> parseMany 2 C <|> parseSingle C
-  c <- rule $ c' <|> c' `andThenAny` [xc, l, xl, x, ix, v, iv, i] <?> C
-
-  cd <- rule $ parseSingle CD <|> parseSingle CD `andThenAny` [xc, l, xl, x, ix, v, iv, i] <?> CD
-
-  d <- rule $ parseSingle D <|> parseSingle D `andThenAny` [c, xc, l, xl, x, ix, v, iv, i] <?> D
-
-  cm <- rule $ parseSingle CM <|> parseSingle CM `andThenAny` [xc, l, xl, x, ix, v, iv, i] <?> CM
-
-  m' <- rule $ parseSome M
-  m <- rule $ m' <|> m' `andThenAny` [cm, d, cd, c, xc, l, xl, x, ix, v, iv, i] <?> M
-
-  s <- rule $ m <|> cm <|> d <|> cd <|> c <|> xc <|> l <|> xl <|> x <|> ix <|> v <|> iv <|> i
-
-  return s
 
 main :: IO ()
 main = do
